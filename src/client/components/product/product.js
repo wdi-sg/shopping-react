8d41c5278e5e2f10ec8adcd2266ee81ce9079e65
angkiki <gabrielangzw@gmail.com>
2018-08-06 21:53:10 +0800

    click on item reveals product

---
+++ src/client/components/product/product.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {hot} from 'react-hot-loader';
+
+class Product extends React.Component {
+  render() {
+    return(
+      <div id='product'>
+        <img src={this.props.product.mediumImage} />
+        <p>
+          {this.props.product.shortDescription}
+        </p>
+        <p>
+          {this.props.product.salePrice}
+        </p>
+        <button type="button">Add To Card</button>
+      </div>
+    )
+  }
+}
+
+export default hot(module)(Product);
--- src/client/components/search/result.js
+++ src/client/components/search/result.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 
+import Product from '../product/product';
+
 class Result extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showProduct: false
+    }
+    this.showProduct = this.showProduct.bind(this);
+  }
+
+  showProduct() {
+    this.setState({
+      showProduct: true
+    })
+  }
+
   render() {
     return (
-      <li>{this.props.results.name} | {this.props.results.salePrice}</li>
+      <div>
+        <li onClick={this.showProduct}>{this.props.results.name} | {this.props.results.salePrice}</li>
+        {this.state.showProduct &&
+          <Product product={this.props.results} />
+        }
+      </div>
     )
   }
 }

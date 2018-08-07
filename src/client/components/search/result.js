573733573544ef0d2b9a0eb3df3e22eab979ebc5
angkiki <gabrielangzw@gmail.com>
2018-08-07 09:16:47 +0800

    shifted product out into search

--- src/client/components/search/result.js
+++ src/client/components/search/result.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 
-import Product from '../product/product';
-
 class Result extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showProduct: false
-    }
-    this.showProduct = this.showProduct.bind(this);
-  }
-
-  showProduct() {
-    this.setState({
-      showProduct: true
-    })
-  }
-
   render() {
     return (
       <div>
-        <li onClick={this.showProduct}>{this.props.results.name} | {this.props.results.salePrice}</li>
-        {this.state.showProduct &&
-          <Product product={this.props.results} />
-        }
+        <li onClick={() => this.props.showProduct(this.props.productId)}>{this.props.results.name} | {this.props.results.salePrice}</li>
       </div>
     )
   }
--- src/client/components/search/search.js
+++ src/client/components/search/search.js
@@ -2,23 +2,53 @@ import React from 'react';
 import {hot} from 'react-hot-loader';
 
 import Result from './result';
+import Product from '../product/product'
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: {
+        visible: false,
+        prodNumber: null
+      }
+    }
+    this.showProduct = this.showProduct.bind(this);
+  }
+
+  showProduct(id) {
+    this.setState({
+      product: {
+        visible: true,
+        prodNumber: id
+      }
+    })
+  }
+
   render() {
     const results = this.props.search.searchResult.map( (results, index) => (
-      <Result key={index} results={results} />
+      <Result key={index} results={results} showProduct={this.showProduct} productId={index} />
     ))
 
     return (
-      <div id='search-block'>
-        <h3>Search</h3>
-        <input onChange={(e) => this.props.handleFormInput(e)} value={this.props.search.searchInput} placeholder="Search Input" />
+      <div className='row'>
+        <div className='col-4'>
+          <div id='search-block'>
+            <h3>Search</h3>
+            <input onChange={(e) => this.props.handleFormInput(e)} value={this.props.search.searchInput} placeholder="Search Input" />
 
-        <br />
-        <button type="button" onClick={this.props.formSubmit}>Search</button>
+            <br />
+            <button type="button" onClick={this.props.formSubmit}>Search</button>
 
-        <br />
-        <ul>{results}</ul>
+            <br />
+            <ul>{results}</ul>
+          </div>
+        </div>
+        <div className='col-4'>
+          {this.state.product.visible &&
+            <Product product={this.props.search.searchResult[this.state.product.prodNumber]} />
+          }
+        </div>
       </div>
     )
   }

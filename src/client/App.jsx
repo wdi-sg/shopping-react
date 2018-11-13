7e001c88b45677d591f8a8606e847a9ab1f48c72
audthenerd <aud.kow88@gmail.com>
2018-11-13 08:20:19 +0800

    ajax up and running

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -5,18 +5,47 @@ import Counter from './components/counter/counter';
 import Form from './components/form/form';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      message: 'hello'
+      entered: "",
+      results: ""
     };
+    this.getInput = this.getInput.bind(this);
+    this.getQuery = this.getQuery.bind(this);
   }
 
+  getInput(event) {
+    this.setState({entered: event.target.value})
+  }
+
+  getQuery(event) {
+    event.preventDefault();
+;
+        const reactThis = this;
+
+        const reqListener = function () {
+            let results = JSON.parse(this.responseText);
+
+            reactThis.setState({results: results});
+        }
+
+        let request = new XMLHttpRequest();
+        request.addEventListener("load", reqListener);
+        request.open("GET", "/api/query?search=" + this.state.search);
+        request.send();
+
+  };
+
+
   render() {
+    console.log('app entered', this.state.entered);
+    console.log('results', this.state.results);
+
     return (
       <div>
-        <Form />
-        Welcome.
+        <h1>Walmart</h1>
+        <Form input={this.getInput} request={this.getQuery} output={this.state.results} />
         <Counter message={this.state.message} />
       </div>
     );
--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -3,18 +3,48 @@ import React from 'react';
 import styles from './style.scss';
 
 class Form extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      monkey: 'haha'
+      button: ""
     };
-  }
+
+  };
+
 
   render() {
+    let products = this.props.output;
+
+    let listItems = products.items.map((item, index) => {
+        return(<ListItem key={index} prod={item} />);
+    });
+
+    console.log('query', this.state.query);
+    console.log('output', this.props.output);
     return (
       <div>
-        <p>{this.state.monkey}</p>
-        <input className={styles.name} />
+        <p>Search for your item</p>
+        <form onSubmit={this.props.request}>
+            <input className={styles.name} onChange={(event) => this.props.input(event)} value={this.state.query} />
+            <button type="submit" disabled={this.state.button} >Search</button>
+        </form>
+        <ul>{listItems}</ul>
+      </div>
+    );
+  }
+}
+
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+
+
+  render() {
+
+    return (
+      <div>
+        <li>{this.props.prod}</li>
       </div>
     );
   }

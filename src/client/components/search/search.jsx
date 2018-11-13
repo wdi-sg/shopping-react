83af6c712438baf82445c55b34f92eaa848e452a
ChloeLiang <nliangxin@gmail.com>
2018-11-13 18:40:23 +0800

    Add style

--- src/client/components/search/search.jsx
+++ src/client/components/search/search.jsx
@@ -11,13 +11,15 @@ const Search = ({searchQuery, items, onChange, onSearch, onSelect, onSort}) => (
     <h2>Search</h2>
     <input className={styles.name} onChange={(e) => onChange(e.currentTarget.value)} value={searchQuery} />
     <button onClick={onSearch}>Search</button>
-    <select onChange={(e) => onSort(e.target.value)} defaultValue="">
-      <option value="" disabled>
-        Sort By...
-      </option>
-      <option value="price">Price</option>
-      <option value="name">Name</option>
-    </select>
+    <div className={styles.sortContainer}>
+      <select className={styles.select} onChange={(e) => onSort(e.target.value)} defaultValue="">
+        <option value="" disabled>
+          Sort By...
+        </option>
+        <option value="price">Price</option>
+        <option value="name">Name</option>
+      </select>
+    </div>
     {items.length !== 0 &&
       items.map((item) => <Item key={item.itemId} itemId={item.itemId} name={item.name} onSelect={onSelect} />)}
   </div>

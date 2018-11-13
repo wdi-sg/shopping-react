dcfd53775e6fc67dfc5fabec913f9ddfb0162623
claraonmars <claraonmars@hotmail.com>
2018-11-14 05:30:55 +0800

    Added sort function

--- src/client/components/search/search.jsx
+++ src/client/components/search/search.jsx
@@ -20,16 +20,31 @@ class Results extends React.Component{
 
 }
 
+class Sort extends React.Component{
+    render(){
+        return(
+            <select onChange={this.props.sortBy} value={this.props.sorted}>
+            <option>Sort By</option>
+            <option value='name'>Name</option>
+            <option value='price'>Price</option>
+            <option value='rating'>Rating</option>
+            </select>
+        )
+    }
+}
+
 class Search extends React.Component {
     constructor() {
         super();
         this.makeQuery = this.makeQuery.bind(this);
         this.changeHandler = this.changeHandler.bind(this);
+        this.sort = this.sort.bind(this);
 
         this.state = {
             banana: 'sneakers',
             queryData: '',
-            queryResult: []
+            queryResult: [],
+            sorted: ''
         };
     }
 
@@ -61,11 +76,65 @@ class Search extends React.Component {
 
     }
 
+    sort(event){
+        this.setState({ sorted: event.target.value });
+        let list = this.state.queryResult;
+
+
+        if (event.target.value === 'name'){
+            let sorted = list.sort(function(a,b) {
+                if (a.name < b.name) {
+                    return -1;
+                  }
+                  if (a.name > b.name) {
+                    return 1;
+                  }
+
+                  // names must be equal
+                  return 0;
+            } );
+            console.log("sorted arr", sorted);
+
+        }
+
+        else if (event.target.value === 'price'){
+            let sorted = list.sort(function(a,b) {
+                if (a.msrp < b.msrp) {
+                    return -1;
+                  }
+                  if (a.msrp > b.msrp) {
+                    return 1;
+                  }
+
+                  // price must be equal
+                  return 0;
+            } );
+            console.log("sorted arr", sorted);
+        }
+
+        else if (event.target.value === 'rating'){
+            let sorted = list.sort(function(a,b) {
+                if (a.customerRating < b.customerRating) {
+                    return -1;
+                  }
+                  if (a.customerRating > b.customerRating) {
+                    return 1;
+                  }
+
+                  // price must be equal
+                  return 0;
+            } );
+            console.log("sorted arr", sorted);
+
+        }
+    }
+
     render() {
         return (
         <div>
             <input onChange={this.changeHandler} value={this.state.queryData} />
             <button onClick={this.makeQuery}>Search</button>
+            <Sort sortBy={this.sort} sorted={this.state.sorted}/>
             <Results item={this.state.queryResult} display={this.props.display}/>
         </div>
         );

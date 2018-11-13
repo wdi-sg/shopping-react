867310971ca99f2787a77527c023c88ed832b305
wandsen <dsenwan@gmail.com>
2018-11-13 09:43:11 +0800

    return data from search

--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -3,21 +3,71 @@ import React from 'react';
 import styles from './style.scss';
 
 class Form extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      monkey: 'haha'
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            search: '',
+            queryData:''
+        };
 
-  render() {
-    return (
-      <div>
-        <p>{this.state.monkey}</p>
-        <input className={styles.name} />
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.searchWalmart = this.searchWalmart.bind(this);
+
+    }
+
+    onChangeHandler(event) {
+        console.log(event.target.value)
+        this.setState({ search: event.target.value })
+    }
+
+    searchWalmart(e) {
+        e.preventDefault()
+        console.log("searching Walmart")
+
+        var reactThis = this;
+
+        function reqListener() {
+            console.log(this.responseText);
+            console.log("search", reactThis.state.search);
+
+            //transform the response to real js objects
+            const data = JSON.parse(this.responseText);
+            console.log("data", data)
+            // here, we can't do this.setState
+
+            //refer to react state instead
+            reactThis.setState({ queryData: data });
+        }
+
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", reqListener);
+
+        oReq.open("GET", "/api/query?search=" + this.state.search);
+        oReq.send();
+
+
+    }
+
+    testfunction(e){
+        e.preventDefault()
+        console.log("working function")
+    }
+
+
+    render() {
+        return (
+            <div>
+        <h1>Search Form</h1>
+
+        <form onSubmit = {this.searchWalmart}>
+        <input onChange={this.onChangeHandler} className={styles.name} placeholder={this.state.search} value={this.state.search} />
+
+        <button type="submit">Submit</button>
+        </form>
+
       </div>
-    );
-  }
+        );
+    }
 }
 
 export default Form;

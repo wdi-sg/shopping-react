24a03d00cd9cb95289e5ec44c97192a10a31b976
hongchiong <hongchiong@gmail.com>
2019-02-18 19:03:56 +0800

    save

--- src/client/components/counter/counter.jsx
+++ src/client/components/counter/counter.jsx
@@ -5,6 +5,33 @@ import PropTypes from 'prop-types';
 import styles from './style.scss';
 import main_styles from '../../style.scss';
 
+const DisplayName = (props) => {
+  return <h6><strong>{props.name}</strong></h6>;
+}
+
+const DisplayImage = (props) => {
+  return <img src={props.img}/>;
+}
+
+const DisplayDescription = (props) => {
+  return <p><small>{props.description}</small></p>;
+}
+
+const DisplayPrice = (props) => {
+  if (props.price) {
+    return <p>${props.price}</p>;
+  } else {
+    return <div></div>;
+  }
+}
+
+const AddToCart = (props) => {
+  if (props.cart) {
+    return <a href={props.cart}>Add To Cart</a>;
+  } else {
+    return <div></div>;
+  }
+}
 class Counter extends React.Component {
   constructor() {
     super();
@@ -12,25 +39,19 @@ class Counter extends React.Component {
       banana: 'sneakers',
       counters : []
     };
-      this.handleClick = this.handleClick.bind(this);
+
   }
 
-    handleClick(){
-        let num = Math.random();
 
-        const newArray = [num, ...this.state.counters];
-
-        this.setState({ counters : newArray });
-    }
 
   render() {
     return (
-      <div>
-          <button onClick={this.handleClick}>click me</button>
-          <p className={styles.desc}>
-            {this.props.message} : {this.state.banana}
-          </p>
-          {this.state.counters.map((counter)=>{ return <p>{counter}</p>})}
+      <div className="col text-center">
+        <DisplayName name={this.props.product.name} />
+        <DisplayImage img={this.props.product.largeImage} />
+        <DisplayDescription description={this.props.product.shortDescription} />
+        <DisplayPrice price={this.props.product.salePrice} />
+        <AddToCart cart={this.props.product.addToCartUrl} />
       </div>
     );
   }

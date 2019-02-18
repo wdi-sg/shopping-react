adfc9ae6afc3d8d5efc5e03b61fc69a8f58dbfb2
ishakbhn <ishakbhn@gmail.com>
2019-02-18 21:41:04 +0800

    add api call at query.js

--- src/server/controllers/query.js
+++ src/server/controllers/query.js
@@ -4,19 +4,22 @@ module.exports = (db) => {
 
   let get = (req, res) => {
 
-    let apiKey = "hello";
+    let apiKey = "bsh6u8q5fdw95yrcy6wkvj75";
 
     let query = req.query.search;
 
-    let url = `http://api.walmartlabs.com/v1/search?apiKey=${apiKey}&query=${query}`
+    let url = `http://api.walmartlabs.com/v1/search?query=${query}&format=json&apiKey=b${apiKey}`
 
     request(url, function (error, queryResponse, body) {
           console.log('error:', error); // Print the error if one occurred and handle it
           console.log('statusCode:', queryResponse && queryResponse.statusCode); // Print the response status code if a response was received
-
-          res.send(body);
+          if (!error && response.statusCode === 200) {
+            res.send(body);
+          } else {
+            res.status(400).send();
+          }
     });
-   }
+  }
 
   return {
     get:get

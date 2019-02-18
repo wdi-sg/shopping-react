28b002f95e1f8a8488269b1075655633a01f4ce0
damienteo <ksteo89@gmail.com>
2019-02-19 00:05:47 +0800

    figured out the issue with showing selected product

--- src/client/components/shopping/ShowResult.jsx
+++ src/client/components/shopping/ShowResult.jsx
@@ -4,17 +4,26 @@ class ShowResult extends React.Component {
 	render () {
 
 		const {showcase} = this.props;
-		console.log(showcase);
+		console.log(showcase[0]);
 
 		return(
 			<div>
+				{ 
+		          showcase[0] !== undefined && 
 		          <React.Fragment>
-			          <h1> Showing Result </h1>
-			          <p>{showcase.name}</p>
+			         	<h1> Showing Result </h1>
+			    		<p> Name: { showcase[0]["name"] }</p>
+			    		<p> Price: { showcase[0]["salePrice"] }</p>
+			    		<img src={ showcase[0]["thumbnailImage"] } />
+			    		<p> Description: { showcase[0]["shortDescription"] }</p>
+			    		<p> Customer Rating: { showcase[0]["customerRating"] }</p>
+			    		<p> Available: { showcase[0]["availableOnline"] }</p>
 		          </React.Fragment>
+		        }
 	        </div>
 		)
 	}
 }
 
 export default ShowResult;
+

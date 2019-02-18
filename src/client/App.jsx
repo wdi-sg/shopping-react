1eda7c5d5c70d9630a665d2263356ea70561021c
angrylobster <liewminshan@gmail.com>
2019-02-18 21:10:17 +0800

    [FRONT END][BACK END] Queries return data to front end as JSON

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-
-import Counter from './components/counter/counter';
 import Form from './components/form/form';
+const axios = require('axios');
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      message: 'hello',
-    };
-  }
+    constructor() {
+        super();
+        this.search = this.search.bind(this);
+        this.state = {
+            message: 'hello',
+        };
+    }
 
-  render() {
-    return (
-      <div>
-        <Form />
-        Welcome.
-        <Counter message={this.state.message} />
-      </div>
-    );
-  }
+    search(searchKey){
+        axios.get('/query', searchKey)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        .then(() => {
+            console.log('done')
+        })
+    }
+
+    render() {
+        return ( 
+            <Form
+                search={ this.search }
+            />
+        );
+    }
 }
 
 export default hot(module)(App);
--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
-
 import styles from './style.scss';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      monkey: 'haha',
-    };
-  }
+    constructor() {
+        super();
+        this.trackInput = this.trackInput.bind(this);
+        this.state = {
+            searchKey: ""
+        };
+    }
 
-  render() {
-    return (
-      <div>
-        <p>{this.state.monkey}</p>
-        <input className={styles.name} />
-      </div>
-    );
-  }
+    trackInput(e){
+        this.setState({
+            searchKey: e.target.value
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <input 
+                    className = { styles.name }
+                    id = "searchKey"
+                    placeholder = "Enter product name"
+                    onChange = { this.trackInput }
+                /> 
+                <button
+                    onClick = { () => { this.props.search(this.state.searchKey) }}
+                >
+                    Search
+                </button>
+            </React.Fragment> 
+
+        );
+    }
 }
+
+Form.propTypes = {
+    search: PropTypes.func
+};
 
 export default Form;

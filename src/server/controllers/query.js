3dea8dc7f4658f73f067f5c548eb6ba39ea83e38
angrylobster <liewminshan@gmail.com>
2019-02-18 16:11:36 +0800

    Initial commit with some formatting edits

--- src/server/controllers/query.js
+++ src/server/controllers/query.js
@@ -1,24 +1,19 @@
 const request = require('request');
-
+const apiKey = "bsh6u8q5fdw95yrcy6wkvj75";
 module.exports = (db) => {
+    let get = (req, res) => {
+        let query = req.query.search;
+        console.log(req.query.search)
+        let url = `http://api.walmartlabs.com/v1/search?apiKey=${apiKey}&query=${query}`
 
-  let get = (req, res) => {
+        request(url, function (error, queryResponse, body) {
+            console.log('error:', error); // Print the error if one occurred and handle it
+            console.log('statusCode:', queryResponse && queryResponse.statusCode); // Print the response status code if a response was received
 
-    let apiKey = "hello";
-
-    let query = req.query.search;
-
-    let url = `http://api.walmartlabs.com/v1/search?apiKey=${apiKey}&query=${query}`
-
-    request(url, function (error, queryResponse, body) {
-          console.log('error:', error); // Print the error if one occurred and handle it
-          console.log('statusCode:', queryResponse && queryResponse.statusCode); // Print the response status code if a response was received
-
-          res.send(body);
-    });
-   }
-
-  return {
-    get:get
-  };
+            res.send(body);
+        });
+    }
+    return {
+        get: get
+    };
 };

2eeee9351eba6c3beea3004a373ac95873a61856
rchuaml <ronchuaml@gmail.com>
2019-02-19 00:29:43 +0800

    made the api call work and added search function which maps out a list of search results and images

--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -5,18 +5,63 @@ import styles from './style.scss';
 class Form extends React.Component {
   constructor() {
     super();
+    this.changeHandler = this.changeHandler.bind( this );
+    this.clickHandler = this.clickHandler.bind( this );
     this.state = {
-      monkey: 'haha',
+        search : "",
+        list: [],
     };
-  }
+}
 
-  render() {
-    return (
-      <div>
-        <p>{this.state.monkey}</p>
-        <input className={styles.name} />
-      </div>
-    );
+changeHandler(event){
+    this.setState({search: event.target.value});
+}
+
+clickHandler(){
+    // console.log(event.target.value)
+        var that = this;
+        // what to do when we recieve the request
+        var responseHandler = function() {
+            that.setState({list:[]})
+          that.setState({search: ""});
+        var searchresult = JSON.parse(this.responseText);
+        var array = that.state.list;
+        for(var i = 0 ; i< searchresult.items.length; i++){
+            array.push(searchresult.items[i]);
+        }
+        that.setState({list:array});
+      };
+
+        // make a new request
+        var request = new XMLHttpRequest();
+
+        // listen for the request response
+        request.addEventListener("load", responseHandler);
+
+        // ready the system by calling open, and specifying the url
+        request.open("GET", `http://localhost:3000/query?search=${this.state.search}`);
+
+        // send the request
+        request.send();
+    }
+
+   render() {
+      // render the list with a map() here
+      console.log("rendering");
+      return (
+        <div>
+            <input onChange={this.changeHandler} value={this.state.search}/>
+            <button className = "search" onClick = {this.clickHandler}>search</button>
+            <div className = "List">
+                <h1>Search for Walmart Products</h1>
+                    <ul>
+                    {this.state.list.map((listitem , index)=>{
+                    return <li> {listitem.name}<img src = {listitem.thumbnailImage}/> </li>
+                    })}
+                    </ul>
+            </div>
+        </div>
+      );
   }
 }
 
--- src/server/controllers/query.js
+++ src/server/controllers/query.js
@@ -4,7 +4,7 @@ module.exports = (db) => {
 
   let get = (req, res) => {
 
-    let apiKey = "hello";
+    let apiKey = "bsh6u8q5fdw95yrcy6wkvj75";
 
     let query = req.query.search;
 
@@ -13,7 +13,6 @@ module.exports = (db) => {
     request(url, function (error, queryResponse, body) {
           console.log('error:', error); // Print the error if one occurred and handle it
           console.log('statusCode:', queryResponse && queryResponse.statusCode); // Print the response status code if a response was received
-
           res.send(body);
     });
    }

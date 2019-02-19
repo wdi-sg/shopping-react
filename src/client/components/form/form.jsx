5f11b8d2d41d8231cc5a6cf571d20ca6da7ad5cd
rchuaml <ronchuaml@gmail.com>
2019-02-19 16:29:50 +0800

    added sort functionality

--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -7,6 +7,7 @@ class Form extends React.Component {
     super();
     this.changeHandler = this.changeHandler.bind( this );
     this.clickHandler = this.clickHandler.bind( this );
+    this.sortHandler = this.sortHandler.bind( this );
     this.state = {
         search : "",
         list: [],
@@ -22,6 +23,7 @@ clickHandler(){
         var that = this;
         // what to do when we recieve the request
         var responseHandler = function() {
+            console.log(this.responseText);
             that.setState({list:[]})
           that.setState({search: ""});
         var searchresult = JSON.parse(this.responseText);
@@ -45,6 +47,37 @@ clickHandler(){
         request.send();
     }
 
+sortHandler(event){
+    let sortInput = event.target.value;
+    if(sortInput === "1"){
+    var sorted = this.state.list;
+    sorted.sort(function(a, b) {
+      if (a["salePrice"] > b["salePrice"]) {
+        return 1;
+      }
+      if (a["salePrice"] < b["salePrice"]) {
+        return -1;
+      }
+      return 0;
+    });
+    this.setState({list:sorted});
+}else if (sortInput === "2"){
+    var sorted = this.state.list;
+     sorted.sort(function(a, b) {
+      if (a["salePrice"] > b["salePrice"]) {
+        return -1;
+      }
+      if (a["salePrice"] < b["salePrice"]) {
+        return 1;
+      }
+      return 0;
+    });
+    this.setState({list:sorted});
+
+}
+
+}
+
    render() {
       // render the list with a map() here
       console.log("rendering");
@@ -52,17 +85,40 @@ clickHandler(){
         <div>
             <input onChange={this.changeHandler} value={this.state.search}/>
             <button className = "search" onClick = {this.clickHandler}>search</button>
+            <select onChange = {this.sortHandler}>
+            <option>Sort Options</option>
+            <option value = "1">Sort by price ascending</option>
+            <option value = "2">Sort by price descending</option>
+            </select>
             <div className = "List">
                 <h1>Search for Walmart Products</h1>
-                    <ul>
-                    {this.state.list.map((listitem , index)=>{
-                    return <li> {listitem.name}<img src = {listitem.thumbnailImage}/> </li>
-                    })}
-                    </ul>
+                <ListItem counter = {this.state.list}/>
             </div>
         </div>
       );
   }
 }
+
+
+    class ListItem extends React.Component {
+        render() {
+            return (
+                <ul className = "list-group">
+                    {this.props.counter.map((listitem , index)=>{
+                    return <li className = "list-group-item"> <img src = {listitem.mediumImage}/>
+                        <h6>
+                            {listitem.name}
+                        </h6>
+                        <p>
+                            {listitem.shortDescription}
+                        </p>
+                        <hr/>
+                        <small>${listitem.salePrice}</small>
+                    </li>
+                    })}
+                </ul>
+            );
+        }
+    }
 
 export default Form;

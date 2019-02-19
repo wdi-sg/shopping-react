518602a7727b20911e5716bb8278201fe5087c57
angrylobster <liewminshan@gmail.com>
2019-02-19 22:57:20 +0800

    [FRONT END] Separate results into 2 classes

--- src/client/components/results/results.jsx
+++ src/client/components/results/results.jsx
@@ -1,45 +1,73 @@
 import React from 'react';
 // import styles from './style.scss';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+var decode = require('decode-html');
 
 class Results extends React.Component {
 
-    sortResults(){
+    mappedResults(){
         if (this.props.results){
-            console.log(this.props.results.data.items)
-            return this.props.results.data.items.map(result => {
+            return this.props.results.data.items.map((result, index) => {
                 return (
-                    <div className="d-flex mt-5 mb-5">
-                        <div className="w-20 mr-3">
-                            <img src={ result.thumbnailImage }/>
-                        </div>
-                        <div>
-                            <h5>{ result.name }</h5>
-                            <div>
-                                <img src={ result.customerRatingImage }/>
-                                <small>{ result.customerRating }</small>
-                            </div>
-                            <small>Reviews: {result.numReviews}</small>
-                            <p>{ result.shortDescription }</p>
-                        </div>
-                    </div>
+                    <Result 
+                        result={ result }
+                        key={ index + result }
+                    />
                 )
             });
+        } else {
+            return (
+                <div>
+                    No results
+                </div>
+            )
         }
-        return "no results"
     }
 
     render() {
         return (
             <div className="w-100">
-                { this.sortResults() }
+                { this.mappedResults() }
             </div> 
         );
     }
 }
 
-// Form.propTypes = {
-//     search: PropTypes.func
-// };
+class Result extends React.Component {
+
+    modifyShortDescription(){
+        if (!this.props.result.shortDescription){
+            return
+        }
+
+        if (this.props.result.shortDescription.length > 200){
+            return decode(this.props.result.shortDescription.substring(0,200)) + "...";
+        }
+        return decode(this.props.result.shortDescription);
+    }
+
+    render() {
+        return (
+            <div className="d-flex mt-5 mb-5">
+                <div className="w-20 mr-3">
+                    <img src={ this.props.result.thumbnailImage }/>
+                </div>
+                <div>
+                    <h5>{ this.props.result.name }</h5>
+                    <div>
+                        <img src={ this.props.result.customerRatingImage }/>
+                        <small>{ this.props.result.customerRating }</small>
+                    </div>
+                    <small>Reviews: { this.props.result.numReviews }</small>
+                    <p>{ this.modifyShortDescription() }</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+Result.propTypes = {
+    result: PropTypes.object
+};
 
 export default Results;

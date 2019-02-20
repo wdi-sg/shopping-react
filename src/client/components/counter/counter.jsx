653b1bd68f4b6d0a58486e637e8098a4999ca251
kingcashthefifth <kingcashthefifth@gmail.com>
2019-02-20 09:58:05 +0800

    experimenting with Fetch, Axios, and manual method for API call

--- src/client/components/counter/counter.jsx
+++ src/client/components/counter/counter.jsx
@@ -10,34 +10,64 @@ class Counter extends React.Component {
     super();
     this.state = {
       banana: 'sneakers',
-      counters : []
+      counters: [],
+      searchResult: []
     };
-      this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-    handleClick(){
-        let num = Math.random();
+  // componentDidMount() {}
 
-        const newArray = [num, ...this.state.counters];
+  handleClick() {
+    fetch('/query')
+      .then((results) => {
+        console.log(results);
+        return results.json();
+      })
+      .then((data) => {
+        this.setState({searchResult: data});
+        console.log(this.state.searchResult);
+      });
 
-        this.setState({ counters : newArray });
-    }
+    // let num = Math.random();
+    // const newArray = [num, ...this.state.counters];
+    // this.setState({counters: newArray});
+    // var compoThis = this;
+    // var responseHandler = function() {
+    //   console.log('response text', this.responseText);
+    //   console.log('status text', this.statusText);
+    //   console.log('status code', this.status);
+    //   const data = JSON.parse(this.responseText);
+    //   console.log(compoThis);
+    //   compoThis.setState({searchResult: data.searchResult});
+    // };
+    // // make a new request
+    // var request = new XMLHttpRequest();
+    // // listen for the request response
+    // request.addEventListener('load', responseHandler);
+    // // ready the system by calling open, and specifying the url
+    // request.open('GET', 'http://api.walmartlabs.com/v1/search?apiKey=bsh6u8q5fdw95yrcy6wkvj75&query=iphone');
+    // // send the request
+    // request.send();
+  }
 
   render() {
     return (
       <div>
-          <button onClick={this.handleClick}>click me</button>
-          <p className={styles.desc}>
-            {this.props.message} : {this.state.banana}
-          </p>
-          {this.state.counters.map((counter)=>{ return <p>{counter}</p>})}
+        <button onClick={this.handleClick}>click me</button>
+        <p className={styles.desc}>
+          {this.props.message} : {this.state.banana}
+        </p>
+        {this.state.counters.map((counter) => {
+          return <p>{counter}</p>;
+        })}
       </div>
     );
   }
 }
 
 Counter.propTypes = {
-  message: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired
 };
 
 export default Counter;

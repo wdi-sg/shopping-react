898c6dbd352937498c193c9ed89fc3ae380cbc8e
sherylwee <wee_mantheng@hotmail.com>
2019-02-20 13:16:24 +0800

    searched

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-import Counter from './components/counter/counter';
+// import Counter from './components/counter/counter';
 import Form from './components/form/form';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      message: 'hello',
     };
   }
+
+
 
   render() {
     return (
       <div>
         <Form />
-        Welcome.
-        <Counter message={this.state.message} />
+        {/* Welcome. */}
+        {/* <Counter message={this.state.message} /> */}
       </div>
     );
   }

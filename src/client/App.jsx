09b8af976aa29af5f131c6237a8135f2a02e5a5b
moses <moseslee90@gmail.com>
2019-05-20 17:26:16 +0800

    linked database and search together with query in input, next step is to filter results by query

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,19 +3,39 @@ import {hot} from 'react-hot-loader';
 
 import Counter from './components/counter/counter';
 import Form from './components/form/form';
+import jsonData from '../../example-result.json';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      message: 'hello world hehe?'
+      message: 'hello world hehe?',
+      data: jsonData,
+      query: '',
+      test: ''
     };
   }
+  onSearchInputChange = (event) => {
+    let searchInput = event.target.value;
+    let newState = {query: searchInput};
+    this.setState(newState);
+  };
+  onSearchSubmit = () => {
+    const query = this.state.query;
+    fetch('/products')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson.products);
+        console.log(query);
+      });
+  };
 
   render() {
     return (
       <div>
-        <Form />
+        <Form onChangeHandler={this.onSearchInputChange} onClickHandler={this.onSearchSubmit} />
         Welcome. boo?
         <Counter message={this.state.message} />
       </div>
--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import styles from './style.scss';
 
 class Form extends React.Component {
   constructor() {
     super();
     this.state = {
-      monkey: 'haha',
+      monkey: 'haha'
     };
   }
 
   render() {
     return (
-      <div>
+      <div onChange={this.props.onChangeHandler}>
         <p>{this.state.monkey}</p>
         <input className={styles.name} />
+        <div className="btn btn-primary" onClick={this.props.onClickHandler}>
+          Search
+        </div>
       </div>
     );
   }
 }
+Form.propTypes = {
+  onChangeHandler: PropTypes.func.isRequired,
+  onClickHandler: PropTypes.func.isRequired
+};
 
 export default Form;

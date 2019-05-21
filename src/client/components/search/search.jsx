b1cdf853b76b6fea181a481e9350c520158213ae
claudia <claucanchin@gmail.com>
2019-05-21 22:54:31 +0800

    refactor moved chunk to App.jsx

--- src/client/components/search/search.jsx
+++ src/client/components/search/search.jsx
@@ -2,125 +2,24 @@ import React from 'react';
 
 import styles from './style.scss';
 
-import Product from '../product/product';
-import Cart from '../cart/cart';
-
 class Search extends React.Component {
     constructor() {
         super();
         this.state = {
-            items: [],
-            currItem: [],
-            cartItems: [],
         };
-        this.makeAjaxCall = this.makeAjaxCall.bind(this);
-        this.getProductInfo = this.getProductInfo.bind(this);
-        this.addToCart = this.addToCart.bind(this);
-    }
-
-    makeAjaxCall() {
-        // get a hold of component for react
-        var reactThis = this;
-
-        var reqListener = function() {
-
-            const data = JSON.parse(this.responseText);
-
-            reactThis.setState({ items: data.products })
-            // console.log(data);
-
-            // this keyword doesnt refer to component
-            //this.setState({items:this.responseText})
-        }
-
-        var oReq = new XMLHttpRequest();
-        oReq.addEventListener("load", reqListener);
-        oReq.open("GET", "/products");
-        oReq.send();
-    }
-
-    getProductInfo(e) {
-        let index = e.target.id;
-        let items = this.state.items;
-        let clickedItem = items[index];
-        let newCurrItem = [];
-
-        newCurrItem.push(clickedItem);
-
-        this.setState({
-            currItem: newCurrItem
-        })
-    }
-
-    addToCart() {
-        let currItem = this.state.currItem;
-        let newCartItems = this.state.cartItems;
-
-        newCartItems.push(currItem);
-        // console.log(newCartItems);
-
-        this.setState({
-            cartItems: newCartItems
-        })
     }
 
     render() {
 
-        const items = this.state.items.map((item, index) => {
-
-            return  <div key={index}
-                        id={index}
-                        onClick={(e) => {this.getProductInfo(e)}}
-                    >
-                    {item.name}
-                    </div>
-        });
         return (
-            <div className={styles.contain}>
-                <div className="row">
-
-                    <div className="col">
-                        <h4>Search</h4>
-                        <input className={styles.searchbox} placeholder="Search Input"/>
-                        <button type="button" className="btn btn-primary" onClick={()=>{this.makeAjaxCall()}}>
-                        Search</button>
-                    </div>
-
-                    <div className="col">
-                        <h4>Product</h4>
-                    </div>
-
-                    <div className="col">
-                        <h4>Cart</h4>
-                    </div>
-
-                </div>
-
-                <div className="row">
-
-                    <div className="col">
-                        {items}
-                    </div>
-
-                    <div className="col">
-                        <Product
-                            clickedItem={this.state.currItem}
-                            addToCart={(e) => {this.addToCart(e)}}
-                        />
-                    </div>
-
-                    <div className="col">
-                        <Cart
-                            cartItems={this.state.cartItems}
-                        />
-                    </div>
-
-                </div>
-
+            <div>
+                <input className={styles.searchbox} placeholder="Search Input"/>
+                <button type="button" className="btn btn-primary" onClick={()=>{this.props.makeAjaxCall()}}>
+                Search</button>
             </div>
-
         );
     }
+
 }
 
 export default Search;

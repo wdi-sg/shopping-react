404e3af5e9983e5458e8997a6e645c5b68baf745
chuasweechin <sampiochua@gmail.com>
2019-05-21 23:43:22 +0800

    bug fix

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -13,7 +13,7 @@ class App extends React.Component {
         this.state = {
             cart: [],
             products: [],
-            selectedProduct: {},
+            selectedProduct: null,
         };
     }
 
--- src/client/components/product/product.jsx
+++ src/client/components/product/product.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 
 class Product extends React.Component {
   render() {
-    return (
-      <div>
-        <h4>Product:</h4>
-        <p>Name: { this.props.selectedProduct.name }</p>
-        <p>Description: { this.props.selectedProduct.description }</p>
-        <p>Price: { this.props.selectedProduct.price }</p>
-        <button
-            onClick= { () => { this.props.addProductToCartHandler() } }>
-            Add to Cart
-        </button>
-      </div>
-    );
+
+    if (this.props.selectedProduct !== null) {
+        return (
+          <div>
+            <h4>Product:</h4>
+            <p>Name: { this.props.selectedProduct.name }</p>
+            <p>Description: { this.props.selectedProduct.description }</p>
+            <p>Price: { this.props.selectedProduct.price }</p>
+            <button
+                onClick= { () => { this.props.addProductToCartHandler() } }>
+                Add to Cart
+            </button>
+          </div>
+        );
+    }
+    else {
+        return (
+          <div>
+            <h4>Product:</h4>
+          </div>
+        );
+    }
+
+
   }
 }
 

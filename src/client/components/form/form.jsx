1a80e4b98a8e142703ff1386a5b149a65d65275c
Yui Terai <yuiterai88@gmail.com>
2019-05-22 14:42:08 +0800

    Rewrite components and Add bootstrap

--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -1,104 +1,20 @@
 import React from 'react';
-import Product from '../product/product';
-
-// import styles from './style.scss';
 
 class Form extends React.Component {
-  constructor() {
-    super();
-    this.timeOut = null;
-    this.state = {
-      searchForm: '',
-      queryData: [],
-      product: {id: '', name: '', price: '', description: ''}
-    };
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-    this.onClickHandler = this.onClickHandler.bind(this);
-    this.showProductHandler = this.showProductHandler.bind(this);
-  }
-
-  onChangeHandler(event) {
-    this.timeOut ? clearTimeout(this.timeOut) : null;
-    this.setState({
-      searchForm: event.target.value
-    });
-    this.timeOut = setTimeout(() => {
-      this.searchItemByName();
-    }, 1000);
-  }
-
-  onClickHandler() {
-    this.searchItemByName();
-  }
-
-  searchItemByName() {
-    fetch(`/searchItems/${this.state.searchForm}`, {
-      method: 'GET'
-    })
-      .then((response) => response.json())
-      .then((json) => this.setState({queryData: json.product}));
-  }
-
-  showProductHandler(event) {
-    let id = event.target.id;
-    let index = this.state.queryData.findIndex(p => p.id == id);
-    let selectedItem = this.state.queryData[index];
-
-
-    this.setState(prevState => ({
-      product: {
-          ...prevState.product,
-          id: selectedItem.id,
-          name: selectedItem.name,
-          price: selectedItem.price,
-          description: selectedItem.description
-      }
-      }));
-  }
-
-  
-
-
 
   render() {
     return (
       <div>
-        <input onChange={this.onChangeHandler} value={this.state.searchForm} placeholder="search" />
-        <button onClick={this.onClickHandler}>Search</button>
-        <ListItems queryData={this.state.queryData} showProductHandler={this.showProductHandler}/>
-        <Product product={this.state.product} />
+        <input onChange={this.props.onChangeHandler} value={this.props.searchForm} placeholder="search" />
+        <button onClick={this.props.onClickHandler}>Search</button>
+        <ListItems queryData={this.props.queryData} showProductHandler={this.props.showProductHandler} />
       </div>
     );
   }
 }
 
-// class ShowItems extends React.Component {
-//   onClickHandler(event) {
-//     console.log('im id: ', this.props.id);
-//     alert('working');
-//   }
-//   // showProductInfo {
-//   //   fetch()
-//   // }
-//   render() {
-//     return(
-//       <button onClick={this.onClickHandler} value={this.props.id}>show</button>
-//     );
-//   }
-// }
-
 class ListItems extends React.Component {
   render() {
-    // let listItems;
-    // if(this.props.queryData.products && this.props.queryData.products.length > 0){
-    //   listItems = this.props.queryData.products.map((item, index) => {
-    //     return (
-    //       <div key={index}>
-    //         <li>{item.name}</li>
-    //       </div>
-    //     );
-    // });
-    // }
     const {queryData} = this.props;
     return (
       <div>
--- src/client/components/product/product.jsx
+++ src/client/components/product/product.jsx
@@ -3,31 +3,23 @@ import React from 'react';
 class Product extends React.Component {
   constructor() {
     super();
-    };
-    render() {
-    //   const selectedItem = this.props.product.map(item => {
-    //         return(
-    //         <ul>                
-    //             <li>
-    //                 {item.name}
-    //             </li>
-    //             <li>
-    //                 {item.price}
-    //             </li>
-    //             <li>
-    //                 {item.description}
-    //             </li>
-    //         </ul>
-    //         )
-    //     });
-        return(
-            <div>{this.props.product.name}</div>
-            // {selectedItem}
-        );
-    }
+  }
+  render() {
+    return (
+        <div>
+            <p>
+                {this.props.product.name}
+            </p>
+            <p>
+                {this.props.product.price}
+            </p>
+            <p>
+                {this.props.product.description}
+            </p>
+            <button>Add to cart</button>
+        </div>
+    );
+  }
 }
-
-
-
 
 export default Product;

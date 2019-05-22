d8525ecc19202b099e87a2a9ed4eb23cb310840d
seeo <aragonlotr5@gmail.com>
2019-05-22 12:32:51 +0800

    added note to remind myself that the App.jsx shall be the parent component. This means that it will hold all the states for the children components

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,6 +4,8 @@ import { hot } from 'react-hot-loader';
 import Counter from './components/counter/counter';
 import Form from './components/form/form';
 
+/* let App.jsx be the parent. Search, Product and Carts shall be the children components */
+
 class App extends React.Component {
   constructor() {
     super();

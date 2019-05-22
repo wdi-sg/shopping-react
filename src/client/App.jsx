392d63f4238620974beb567cf1fdbebabb3737ae
seeo <aragonlotr5@gmail.com>
2019-05-22 12:44:54 +0800

    created a search component boiler plate

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,7 +4,9 @@ import { hot } from 'react-hot-loader';
 import Counter from './components/counter/counter';
 import Form from './components/form/form';
 
-/* let App.jsx be the parent. Search, Product and Carts shall be the children components */
+/* let App.jsx be the parent. Search, Product and Carts shall be the
+children components. This means the only place where you see this.state
+is inside the parent, App.jsx class */
 
 class App extends React.Component {
   constructor() {
---
+++ src/client/components/search/search.jsx
@@ -0,0 +1,18 @@
+/*search component is a child component,
+so it does not require the ReactDOM.render... */
+
+/* Though we still need to import the react library so that the computer
+can understand jsx syntax*/
+
+import React from 'react';
+
+class Search extends React.Component{
+    render(){
+        return (
+            <React.Fragment>
+
+
+            </React.Fragment>
+        )
+    }
+}

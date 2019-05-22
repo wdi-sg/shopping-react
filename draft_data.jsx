d3ba9260c4a184ccd8acc191e4d27c2392f11b82
moses <moseslee90@gmail.com>
2019-05-22 23:00:49 +0800

    added draft data to visualise transformation of data received from server

--- draft_data.jsx
+++ draft_data.jsx
@@ -5,5 +5,16 @@ let formObject = {
     address_fields: [
         {id: 1, form_id: 1, title: 'address', position: 3} //get from google maps
     ],
-    email_fields: [{id: 1, form_id: 1, title: 'email', position: 4}]
+    email_fields: [{id: 1, form_id: 1, title: 'email', position: 5}],
+    phone_fields: [{id: 1, form_id: 1, title: 'handphone', position: 4}]
 };
+//flatten object
+let formArray = [
+    {
+        text_field_id: 1,
+        form_id: 1,
+        title: 'name',
+        position: 1,
+        field_type: 'text'
+    }
+];

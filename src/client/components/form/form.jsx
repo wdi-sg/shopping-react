b290bf4e70743a2431506a47f18ad29164006bf1
gitwenlei <ngwenlei@gmail.com>
2019-09-02 17:50:43 +0800

    Able to display all items in products database.

--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -2,19 +2,54 @@ import React from 'react';
 
 import styles from './style.scss';
 
+
+class List extends React.Component {
+    render() {
+        let itemsElements = this.props.items.map((item, index) => {
+          return <li key={index}>{item.name}</li>
+        });
+        return (
+          <ul>
+            {itemsElements}
+          </ul>
+        );
+    }
+}
+
+
+
 class Form extends React.Component {
   constructor() {
     super();
     this.state = {
-      monkey: 'haha',
+      searchText: '',
+        products: []
     };
   }
 
+
+  componentDidMount() {
+    fetch('http://localhost:3000/products')
+      .then(data => data.json())
+      .then((data) => { this.setState({ products: data.products }) });
+
+      // const productsList = this.state.products.map;
+  }
+
+
+  changeHandler(event) {
+    let currentValue = event.target.value;
+    this.setState({searchText: currentValue});
+    console.log("value: ", currentValue);
+    // currentValue = this.state.searchText;
+  }
+
   render() {
-    return (
+    return(
       <div>
-        <p>{this.state.monkey}</p>
-        <input className={styles.name} />
+        <h3>Search:</h3>
+        <input className={styles.name} onChange={(event)=>{this.changeHandler(event);}} value={this.state.searchText}/>
+        <List items={this.state.products} />
       </div>
     );
   }

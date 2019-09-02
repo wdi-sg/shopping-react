b5b16c7879ee234bddb53be27989c0f489ba511f
artylope <angyixin@gmail.com>
2019-09-03 07:01:19 +0800

    Able to add and show items

--- src/client/components/cart/cart.jsx
+++ src/client/components/cart/cart.jsx
@@ -4,13 +4,40 @@ import styles from '../../style.scss';
 
 class Cart extends React.Component{
 
-    render(){
-        
-        return(
-            <div className={styles.cart}>Cart</div>
-        )
-    }
 
+    render(){
+        console.log('in cart Component');
+        console.log(this.props.cartItems)
+
+        if(this.props.cartItems){
+
+            let cartItemList = this.props.cartItems.map((item) => {
+                return(
+                    <tr>
+                        <td>{item.name}</td><td>{item.price}</td>
+                    </tr>
+
+                )
+            })
+
+
+            return(
+                <div className={styles.cart}>
+                    <h1>Cart</h1>
+                    <table>
+                        {cartItemList}
+                    </table>
+                </div>
+            )
+        } else{
+            return(
+                <div className={styles.cart}>
+                    <h1>Cart</h1>
+                </div>
+            )
+        }
+
+    }
 
 
 }

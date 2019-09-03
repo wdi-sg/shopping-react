95d37f052bc602a4675660698665b3db2c36e205
gitwenlei <ngwenlei@gmail.com>
2019-09-03 15:10:34 +0800

    Search & display items based on user entered keywords

--- src/client/components/form/form.jsx
+++ src/client/components/form/form.jsx
@@ -19,12 +19,15 @@ class List extends React.Component {
 
 
 class Form extends React.Component {
+
   constructor() {
     super();
     this.state = {
       searchText: '',
-        products: []
+        products: [],
+        foundList: []
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
 
@@ -32,23 +35,52 @@ class Form extends React.Component {
     fetch('http://localhost:3000/products')
       .then(data => data.json())
       .then((data) => { this.setState({ products: data.products }) });
-
-      // const productsList = this.state.products.map;
   }
 
 
   changeHandler(event) {
-    let currentValue = event.target.value;
+    let currentValue = this.state.searchText;
+    currentValue = event.target.value;
     this.setState({searchText: currentValue});
-    console.log("value: ", currentValue);
-    // currentValue = this.state.searchText;
+    console.log("state: ", this.state.searchText);
   }
+
+    handleClick(){
+        // impt to bind function in constructor in order to call this.state keys.
+        // alert(this.state.searchText);
+
+        // return all product objects inside array
+        // let all = this.state.products.map((item) => {
+            // console.log(item);
+        // });
+        // let foundItems = this.state.foundList;
+        let itemArray = this.state.products;
+        let word = this.state.searchText;
+        let foundItems = [];
+
+        itemArray.map(item => {
+            // console.log(item.name);
+            // console.log (item.name.toLowerCase().includes(word));
+
+            if (item.name.toLowerCase().includes(word.toLowerCase())) {
+                console.log("*************");
+                console.log("found:", item.name);
+                foundItems.push(item.name);
+                this.setState({foundList: foundItems});
+            }
+        });
+
+        console.log(foundItems);
+
+    }
+
 
   render() {
     return(
       <div>
         <h3>Search:</h3>
         <input className={styles.name} onChange={(event)=>{this.changeHandler(event);}} value={this.state.searchText}/>
+        <button onClick={this.handleClick}>search</button>
         <List items={this.state.products} />
       </div>
     );

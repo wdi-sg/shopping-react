79368384d9fdec1a670569d7b1fa8397c67c0c20
David M <davidmosk@gmail.com>
2019-11-25 20:16:20 +0800

    got json working to display in products

---
+++ app/javascript/components/form.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class Form extends React.Component {
+  constructor () {
+  super ()
+    this.state = {
+      posts: []
+    }
+  }
+    getPosts(){
+
+      const url = '/products.json';
+    
+      axios.get(url)
+        .then((response) => {
+    
+          const data = response.data
+    
+          this.setState({ posts: data })
+    
+        }).catch((error)=>{
+          console.log(error);
+        })
+    }
+
+
+    render() {
+      // console.log( "rendering", this.state.posts);
+      // console log the posts, this should be an array of objects
+const posts = this.state.posts.map((post, index)=>{
+  return (<div key={index}>
+    <p>{post.title}</p>
+    <p>{post.content}</p>
+  </div>);
+});
+  
+      
+      return (
+        <div>
+         Hello App
+
+        <button onClick={()=>{ this.getPosts() }}>
+        Click to See Posts
+        </button>
+        {posts}
+        </div>
+      );
+    }
+  }
+  
+  export default Form;

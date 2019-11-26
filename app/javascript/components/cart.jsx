035a28fc67c4b3a47e1e2d3247f0911f87af7377
SYNG <simyenng@gmail.com>
2019-11-26 19:05:19 +0800

    Able to total cart amount

--- app/javascript/components/cart.jsx
+++ app/javascript/components/cart.jsx
@@ -8,9 +8,14 @@ class Cart extends React.Component{
     render(){
       // receiving products from Search.jsx
       console.log(this.props.checkout)
+      let subtotal = 0;
       const products = this.props.checkout.map((product) => {
+
+        subtotal += parseFloat(product.price);
         return(
+          <div>
             <p>{product.name}: ${product.price}</p>
+          </div>
         );
       })
 
@@ -19,6 +24,8 @@ class Cart extends React.Component{
           <div>
             <h3>Cart</h3>
             {products}
+            <hr/>
+            <strong>Subtotal: {subtotal.toFixed(2)}</strong>
           </div>
         )
     }

96c2642ae79cb2be8ce80a3c31524a2a82daff7f
kevinngth <kevin.ng.th@gmail.com>
2019-11-26 22:43:41 +0800

    attempt to fix bug

--- app/javascript/components/Search/Search.jsx
+++ app/javascript/components/Search/Search.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 
 class Search extends React.Component{
     render() {
@@ -21,7 +23,18 @@ class Search extends React.Component{
                 label="Search"
                 variant="outlined"
                 onChange={(e)=>this.props.onSearchQuery(e)}
-            />
+                className="btn-block"
+            /><br/>
+            <Select
+                labelId="demo-simple-select-outlined-label"
+                id="demo-simple-select-outlined"
+                onChange={(e)=>{console.log(e.target.value)}}
+                className="btn-block my-1"
+            >
+                <MenuItem value=""><em>None</em></MenuItem>
+                <MenuItem value={"name"}>Name</MenuItem>
+                <MenuItem value={"price"}>Price</MenuItem>
+            </Select>
             <div className="mt-2">
                 {results}
             </div>

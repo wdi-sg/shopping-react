75fa446afe61a24aac81c7ac477b859e6c2a0dfa
SYNG <simyenng@gmail.com>
2019-11-26 19:14:14 +0800

    Added gst & shipping

--- app/javascript/components/cart.jsx
+++ app/javascript/components/cart.jsx
@@ -9,6 +9,8 @@ class Cart extends React.Component{
       // receiving products from Search.jsx
       console.log(this.props.checkout)
       let subtotal = 0;
+      const gst = 0.07;
+      const shipping = 7;
       const products = this.props.checkout.map((product) => {
 
         subtotal += parseFloat(product.price);
@@ -25,7 +27,10 @@ class Cart extends React.Component{
             <h3>Cart</h3>
             {products}
             <hr/>
-            <strong>Subtotal: {subtotal.toFixed(2)}</strong>
+            <p>Subtotal: ${subtotal.toFixed(2)}</p>
+            <p>GST: ${(subtotal*gst).toFixed(2)}</p>
+            <p>Shipping: ${shipping}</p>
+            <strong>Total: {(subtotal+(subtotal*gst)+shipping).toFixed(2)}</strong>
           </div>
         )
     }

9bf08b6d08c4229ef71436f74cbab621fb740910
joycemap <57893220+joycemap@users.noreply.github.com>
2020-03-02 21:57:00 +0800

    Components all show and buttons for each item

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -11,9 +11,16 @@ class Products extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: []
+      products: [],
+      product: {}
     };
+    this.getProducts = this.getProducts.bind(this);
   }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
   getProducts() {
     console.log("YAY");
     const url = "/products.json";
@@ -22,8 +29,21 @@ class Products extends React.Component {
       .get(url)
       .then(response => {
         const data = response.data;
+        this.setState({ products: data, product: {} });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-        this.setState({ products: data });
+  getsingleProduct(id) {
+    const url = "/products/" + id + ".json";
+
+    axios
+      .get(url)
+      .then(response => {
+        const data = response.data;
+        this.setState({ products: [], product: data });
       })
       .catch(error => {
         console.log(error);
@@ -38,32 +58,80 @@ class Products extends React.Component {
       console.log(products);
       return (
         <div>
-          <img src={products.image_url} />
-          <div>
-            <strong>Name:</strong>
-            <p>{products.name}</p>
-            <strong>Description:</strong>
-            <p>{products.description}</p>
-            <strong>Price($):</strong>
-            <p>{products.price}</p>
-          </div>
+          <strong>Name:</strong>
+          <p>{products.name}</p>
+          <button
+            onClick={() => {
+              this.getsingleProduct(products.id);
+            }}
+          >
+            Click to see this Product
+          </button>
         </div>
       );
     });
+
     return (
       <div>
         <h2>All Products</h2>
         {products}
+        <Oneproduct
+          product={this.state.product}
+          getProducts={this.getProducts}
+        />
 
-        <button
+        {/* <button
           onClick={() => {
             this.getProducts();
           }}
         >
           Click to See Products
-        </button>
+        </button> */}
       </div>
     );
+  }
+}
+
+export default class Oneproduct extends React.Component {
+  getProducts() {
+    this.props.getProducts();
+  }
+  render() {
+    let product = this.props.product;
+    if (Object.keys(product).length > 0) {
+      return (
+        <div>
+          <img src={product.image_url} />
+          <div>
+            <strong>Name:</strong>
+            <p>{product.name}</p>
+            <strong>Description:</strong>
+            <p>{product.description}</p>
+            <strong>Price($):</strong>
+            <p>{product.price}</p>
+          </div>
+
+          <button
+            onClick={() => {
+              this.getProducts();
+            }}
+          >
+            Click to see all Products
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div></div>
+        // <button
+        //   onClick={() => {
+        //     this.getProducts();
+        //   }}
+        // >
+        //   Click to see all Products
+        // </button>
+      );
+    }
   }
 }
 

e849b41b5abe192ee59a9088208799e4459194fa
joycemap <57893220+joycemap@users.noreply.github.com>
2020-03-02 20:29:17 +0800

    Show all products done

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -2,25 +2,84 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import axios from "axios";
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+class Products extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: []
+    };
+  }
+  getProducts() {
+    console.log("YAY");
+    const url = "/products.json";
+
+    axios
+      .get(url)
+      .then(response => {
+        const data = response.data;
+
+        this.setState({ products: data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    console.log("RENDERRRRRING");
+    console.log(this.state.products);
+
+    const products = this.state.products.map((products, index) => {
+      console.log(products);
+      return (
+        <div>
+          <img src={products.image_url} />
+          <div>
+            <strong>Name:</strong>
+            <p>{products.name}</p>
+            <strong>Description:</strong>
+            <p>{products.description}</p>
+            <strong>Price($):</strong>
+            <p>{products.price}</p>
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div>
+        <h2>All Products</h2>
+        {products}
+
+        <button
+          onClick={() => {
+            this.getProducts();
+          }}
+        >
+          Click to See Products
+        </button>
+      </div>
+    );
+  }
+}
+
+const Hello = props => <div>Hello {props.name}!</div>;
 
 Hello.defaultProps = {
-  name: 'David'
-}
+  name: "David"
+};
 
 Hello.propTypes = {
   name: PropTypes.string
-}
+};
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    <Products name="React" />,
+    document.body.appendChild(document.createElement("div"))
+  );
+});

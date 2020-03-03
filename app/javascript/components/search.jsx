c9d9d3e4f01951e0f03008d9477e801c930b810b
Stuart Myers <stuart.myers@gmail.com>
2020-03-03 13:59:44 +0800

    Added pagination

--- app/javascript/components/search.jsx
+++ app/javascript/components/search.jsx
@@ -7,6 +7,15 @@ import TableCell from '@material-ui/core/TableCell'
 import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
 export default class Search extends Component {
     constructor() {
@@ -64,49 +73,152 @@ class SearchBox extends Component {
 }
 
 
-class ProductList extends Component {
+function ProductList(props) {
 
-    onItemClick(event) {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, props.products.length - page * rowsPerPage);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = event => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const onItemClick = (event) => {
         let number = event.target.id
-        this.props.onItemClick(number)
+        props.onItemClick(number)
     }
 
-    render() {
-
-        let productList = []
-        if (this.props.products.length > 0) {
-            productList = this.props.products.map((item, index) => {
-                return (
-                    <TableRow id={item.id} 
-                    key={index} 
-                    onClick={(event)=>{this.onItemClick(event)}}>
-                        <TableCell id={item.id}>
-                            {item.name}
-                        </TableCell>
-                        <TableCell id={item.id} align="right">
-                            ${item.price}
-                        </TableCell>
-                    </TableRow>
-                )
-            }
+    let productList = []
+    let productsToShow = []
+    if (props.products.length > 0) {
+        productsToShow = props.products.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+        productList = productsToShow.map((item, index) => {
+            return (
+                <TableRow id={item.id} 
+                key={index} 
+                onClick={(event)=>{onItemClick(event)}}>
+                    <TableCell id={item.id}>
+                        {item.name}
+                    </TableCell>
+                    <TableCell id={item.id} align="right">
+                        ${item.price}
+                    </TableCell>
+                </TableRow>
             )
-    }
-
-        return (
-            <TableContainer>
-                <Table size="small" aria-label="search-results">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell align="right">Price</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {productList}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-                
+        }
         )
     }
+
+    return (
+        <TableContainer>
+            <Table size="small" aria-label="search-results">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {productList}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination 
+                        rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                        colSpan={2}
+                        count={props.products.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        SelectProps={{
+                            inputProps: { 'aria-label': 'rows per page' },
+                            native: true,
+                        }}
+                        onChangePage={handleChangePage}
+                        onChangeRowsPerPage={handleChangeRowsPerPage}
+                        ActionsComponent={TablePaginationActions} />
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </TableContainer>
+            
+    )
+}
+
+const useStyles2 = makeStyles({
+    table: {
+      minWidth: 500,
+    },
+  });
+
+
+const useStyles1 = makeStyles(theme => ({
+root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+},
+}));
+
+TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onChangePage: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+  };
+
+
+function TablePaginationActions(props) {
+    const classes = useStyles1();
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onChangePage } = props;
+  
+    const handleFirstPageButtonClick = event => {
+      onChangePage(event, 0);
+    };
+  
+    const handleBackButtonClick = event => {
+      onChangePage(event, page - 1);
+    };
+  
+    const handleNextButtonClick = event => {
+      onChangePage(event, page + 1);
+    };
+  
+    const handleLastPageButtonClick = event => {
+      onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+  
+    return (
+      <div className={classes.root}>
+        <IconButton
+          onClick={handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="first page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="last page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </div>
+    )
 }

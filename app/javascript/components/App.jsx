bd58daf9418328b3450c6338b810729001a60a97
joyce-paul <joyce_paul@hotmail.com>
2020-03-03 15:49:42 +0800

    Added bootstrap and some minor styling.

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios';
 import Products from '../components/Products';
+// import Carts from '../components/Carts';
 
+var popups = require("popups");
 
 class App extends React.Component {
     constructor() {
@@ -16,12 +18,9 @@ class App extends React.Component {
 
 getProducts(){
   const url = '/products.json';
-
   axios.get(url)
     .then((response) => {
-
       const data = response.data
-
       this.setState({ products: data })
       console.log(this.state.products)
   }).catch((error)=>{
@@ -35,9 +34,9 @@ showIndividualProduct(index) {
 }
 
 render() {
-    let lists = this.state.products.map( (stuff, index)=> {
+    let lists = this.state.products.map((stuff, index)=> {
         return (
-        <button value={index} onClick={e => {
+        <button className="btn btn-outline-info" value={index} onClick={e => {
             this.showIndividualProduct(e.target.value)
         }}>{stuff.name}</button>
         )
@@ -45,18 +44,33 @@ render() {
 
     return (
           <div>
-          <button onClick={()=>{ this.getProducts() }}>
-          Click to See Products
+          <button className="btn btn-danger" onClick={()=>{ this.getProducts() }}>
+          Click to view products
           </button>
-        <ul>
-                {lists}
+        <ul style={{backgroundColor: "lightyellow"}}>
+        {lists}
         </ul>
         <Products individualProduct = {this.state.individualProduct} />
-
 </div>
         )
     }
 }
+
+
+
+
+// addProductToCart(){
+//     if (selectedProduct = this.state.products[index]) {
+//         this.state.products.push(this.state.cart);
+//         this.setState(this.state.products);
+//         console.log("clicking")
+//     } else {
+//         popups.alert({
+//             content:
+//                 "Please select an item"
+//             });
+//     }
+// }
 
 
 

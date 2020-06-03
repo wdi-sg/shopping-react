0eebcd1bf0779533f49ad4d0de88767a4c6ab8aa
lekhweemeng <lek.hweemeng@live.com>
2020-06-03 23:28:26 +0800

    added in buttons for add to cart

--- app/javascript/components/product.jsx
+++ app/javascript/components/product.jsx
@@ -6,12 +6,59 @@ class Product extends React.Component {
       super();
 
       this.state = {
-
+        products:[]
       };
     }
+
+    handleClick(){
+
+      console.log("hey wowwww")
+
+
+      const url = '/products.json';
+
+      const runWhenDone = (response) => {
+
+        const data = response.data
+        console.log("**************")
+        console.log("**************")
+        console.log("**************")
+        console.log("**************")
+        console.log( data );
+
+        this.setState({ products: data })
+
+      }
+
+      const whenError = (error) => {
+          console.log("eerror", error)
+      }
+
+      axios.get(url).then(runWhenDone).catch(whenError)
+    }
+
     render(){
+
+        const products = this.state.products.map((product)=>{
+            return (<div className="product">
+                <img src={product.url}/>
+                <p>Product Name: {product.name}</p>
+                <p>Product description: {product.description}</p>
+                <p>Product Price: ${product.price}</p> <button>add to cart</button>
+            </div>);
+        });
         return (<div className="col">
-            <p>This is where product is showing</p>
+        <p>Product list:</p>
+        <p>
+            <button onClick={()=>{
+                this.handleClick();
+            }}>
+                get list of Products from db
+            </button>
+        </p>
+        <div>
+            {products}
+        </div>
         </div>);
     }
 }

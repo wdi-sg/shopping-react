77c4a86a8174d2bd3cbaef96b9bc1e5be4e640ab
lekhweemeng <lek.hweemeng@live.com>
2020-06-03 22:46:19 +0800

    created devise, registered users, added products

---
+++ db/migrate/20200603143708_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :url
+      t.text :name
+      t.text :description
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
+end

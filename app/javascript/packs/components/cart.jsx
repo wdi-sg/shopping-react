d9fdd5ef279e742af3809bc3d4feeb99aca39821
kenneththesheep <kenneththesheep@gmail.com>
2020-06-04 03:12:52 +0800

    have some mathematics

--- app/javascript/packs/components/cart.jsx
+++ app/javascript/packs/components/cart.jsx
@@ -61,7 +61,28 @@ class Cart extends React.Component {
         Total Cost:
         </div>
         <div className = "col-4">
-        $ {this.props.cartPriceArray.reduce((a, b) => a + b, 0).toFixed(2)}
+        Subtotal: $ {this.props.cartPriceArray.reduce((a, b) => a + b, 0).toFixed(2)}
+
+        </div>
+        <div className = "col-8">
+        Delivery (Flat Rate):
+        </div>
+        <div className = "col-4 text-right pr-5">
+        + $ 7
+        </div>
+        <div className = "col-8">
+        GST:
+        </div>
+        <div className = "col-4 text-right pr-5">
+        +$ {(this.props.cartPriceArray.reduce((a, b) => a + b, 0)*0.07).toFixed(2)}
+
+        </div>
+        <div className = "col-8">
+        Grandtotal:
+        </div>
+        <div className = "col-4 text-right pr-5">
+        $ {(this.props.cartPriceArray.reduce((a, b) => a + b, 0)*1.07 +7).toFixed(2)}
+
         </div>
     </div>
     );

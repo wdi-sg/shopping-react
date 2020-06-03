506409b8157b6657e7d2785b2ed0da18abe04ba9
chelsejw <chelsejw@gmail.com>
2020-06-04 01:24:35 +0800

    install ffaker, add description column, setup app component

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_171557) do
+ActiveRecord::Schema.define(version: 2020_06_03_172324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_171557) do
     t.string "img_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
 end

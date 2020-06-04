b3fc6947289b2ee0e187969cf0cce1fc87b9431a
vigbit <vigneshb@live.com>
2020-06-04 08:21:57 +0800

    "db seed ffaker"

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -5,7 +5,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import App from './components/app'
+import App from '../components/app'
 
 // const Hello = props => (
 //   <div>Hello {props.name}!</div>

b81d6c86340c72aacdecfe1edbaa966594bd9145
travisenquiry <travisenquiry@gmail.com>
2020-06-06 13:08:30 +0800

    figured out logic to use one view page only

--- app/controllers/onepage_controller.rb
+++ app/controllers/onepage_controller.rb
@@ -1,4 +1,10 @@
 class OnepageController < ApplicationController
   def index
+    @current_page = "index"
+  end
+
+  def product
+    @cureent_page = "product"
+    render 'index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'onepage/index'
+  get 'onepage/index' => 'onepage#index'
+  get 'onepage/product' => 'onepage#product'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

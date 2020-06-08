6ed86b1167837a7d001429a4e47d5a9b7d018694
YusofGotboudine <yusof95@hotmail.com>
2020-06-08 10:23:22 +0800

    Working React Component

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -4,23 +4,15 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+
 
 const Hello = props => (
-  <div>Hello {props.name}!</div>
+  <div>Hello React!</div>
 )
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Hello />,
     document.body.appendChild(document.createElement('div')),
   )
 })
